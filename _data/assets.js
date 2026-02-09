bdce601965a9aecb51327302d1506e5f89b8cdcb
Sudheer Gudi <sudheer@bullattorneys.com>
2026-02-09 16:50:35 -0600

    Deleted all unnecessary things from the template

--- _data/assets.js
+++
@@ -1,9 +0,0 @@
-const folder = "images/gallery";
-const hero = ["1-yacht.jpg", "2-yacht.jpg", "3-yacht.jpg"];
-
-module.exports = {
-  folder,
-  hero,
-  heroUrls: hero.map((f) => `/${folder}/${f}`),
-  list: [],
-};
