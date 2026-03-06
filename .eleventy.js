68d5e689672d395b8357ff977889429ce12eba1a
Sudheer Gudi <sudheer@bullattorneys.com>
2026-03-06 10:09:44 -0600

    sitemap, canonical links and robots.txt file updated

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("CNAME");
 
   return {
     pathPrefix: "",
